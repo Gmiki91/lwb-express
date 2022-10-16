fa891159c55b0657b8baa2b5f00ece4deaa8dd94
Miklos Gabor <gmiki91@gmail.com>
2022-10-16 14:23:04 +0200

    not sending back whole user

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ const createSendToken = (user, statusCode, res) => {
     res.status(statusCode).json({
         status: 'success',
         token,
-        user
+        type:user.type
     });
 };
 
